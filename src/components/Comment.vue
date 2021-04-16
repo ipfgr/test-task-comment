e076f275192887674499571ab482bc5b2ccfd6f4
ipfgr <55628701+ipfgr@users.noreply.github.com>
2021-04-16 23:08:48 +0700

    Add modal hide

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,64 +1,70 @@
 <template>
-<div class="blocker" @click="resetHandler"></div>
-        <div class="card">
-            <div class="comment"><span>{{comment.comment}}</span></div>
-            <div class="info">
-                <div class="icon">
-                    <img src="../assets/noavatar.png" >
-                </div>
-                <div class="name">
-                    <h3>{{comment.user}}</h3>
-                </div>
-                <div class="date">
-                    <h3>{{comment.date}}</h3>
-                </div>
-                <div @click.stop="modalTrigger" class="modal-open">
-                        <img class="arrow-down" src="../assets/drop-down-arrow.svg" >
-                    <Modal @modal-trigger="triggersHandler"/>
+    <div class="card">
+        <div class="comment"><span>{{comment.comment}}</span></div>
+        <div class="info">
+            <div class="icon">
+                <img src="../assets/noavatar.png">
+            </div>
+            <div class="name">
+                <h3>{{comment.user}}</h3>
+            </div>
+            <div class="date">
+                <h3>{{comment.date}}</h3>
+            </div>
+            <div @click.stop="modalTrigger" class="modal-open">
+                <img class="arrow-down" src="../assets/drop-down-arrow.svg">
+                <div>
+                    <div class="modal" ref="modal">
+                        <div @click="editComment" class="edit-modal buttons">Edit comment</div>
+                        <div @click="deleteComment" class="delete-modal buttons">Delete comment</div>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
 </template>
 
 <script>
-    import Modal from "@/components/Modal"
+    // import Modal from "@/components/Modal"
 
     export default {
         name: "Comment.vue",
-        components:{
-          Modal,
-        },
+        components: {},
         props: {
             comment: Object
         },
         data() {
-            return{
+            return {
+                modalVisible: false
             }
         },
-        methods:{
-             //Close modal window when user press outside modal
-            resetHandler(){
-                const blocker = document.querySelector(".blocker")
-                blocker.style.display = "none"
-                const modal = document.querySelector(".modal")
-                modal.style.display = "none"
+        computed: {},
+        methods: {
 
+            editComment() {
+
+                this.$emit("modal-trigger", "edit", this.comment.id)
             },
-           // Transfer emmit to from modal to parent
-           triggersHandler(task){
-               if(task === "delete"){
-                 this.$emit("modal-trigger","delete",this.comment.id)
-               }
-               if (task === "edit"){
-                   this.$emit("modal-trigger","edit",this.comment.id)
-               }
+            deleteComment() {
+                if (confirm(`Are you sure that's want delete this comment ?`)) {
+
+                    this.$emit("modal-trigger", "delete", this.comment.id)
+                }
+            },
+            // Transfer emmit to from modal to parent
+            triggersHandler(task) {
+                if (task === "delete") {
+                    this.$emit("modal-trigger", "delete", this.comment.id)
+                }
+                if (task === "edit") {
+                    this.$emit("modal-trigger", "edit", this.comment.id)
+                }
             },
             //Show modal window
-            modalTrigger(){
+            modalTrigger() {
                 const blocker = document.querySelector(".blocker")
                 blocker.style.display = "block"
-                const modal = document.querySelector(".modal")
-                modal.style.display = "block"
+                this.$refs.modal.style.display = "block"
             },
 
         }
@@ -66,57 +72,76 @@
 </script>
 
 <style scoped>
-    *{
+    * {
         line-height: 2;
-
     }
 
-    .blocker{
-            display:none;
-            background-color: grey;
-            opacity: 0.3;
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            z-index: 10;
-            height: 100%;
-        }
 
-    .card{
-    position:relative;
+    .card {
+        position: relative;
     }
-    .comment{
+
+    .comment {
         font-size: 16px;
-
     }
-    .info{
+
+    .info {
         color: #a5a5a5;
-        display:flex;
+        display: flex;
         align-items: center;
         font-size: 10px;
     }
-    .name{
+
+    .name {
         margin: 0px 5px;
     }
-    .icon{
 
-    }
-    .icon img{
-        width:12px;
-        height:12px;
-        margin-right:5px;
+    .icon img {
+        width: 12px;
+        height: 12px;
+        margin-right: 5px;
     }
 
-    .modal-open{
+    .modal-open {
         cursor: pointer;
-        padding-left:5px;
+        padding-left: 5px;
     }
 
-    .arrow-down{
+    .arrow-down {
         width: 12px;
         height: 12px;
     }
 
+    /*  Modal  */
+    .modal {
+        z-index: 100;
+        position: absolute;
+        right: 10%;
+        display: none;
+        flex-direction: column;
+        justify-content: center;
+        width: 200px;
+        height: 100px;
+        background-color: #fff;
+        border-radius: 5px;
+        color: #808080;
+    }
+
+    .buttons {
+        align-items: center;
+        height: 50%;
+        display: flex;
+        padding-left: 5px;
+        transition: all 0.2s
+    }
+
+    .buttons:hover {
+        background-color: #f1f0f0;
+        align-items: center;
+        height: 50%;
+        display: flex;
+        padding-left: 6px;
+        cursor: pointer;
+    }
 
 </style>
--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="comments-body">
+        <div @click="resetHandler" class="blocker"></div>
 
         <div class="wrapper">
             <div class="comments-header">
@@ -14,18 +15,21 @@
             <div class="line"></div>
             <div class="comments-add">
                 <div class="add-comment">
-                    <input type="text" @focus="showButtons" class="form" placeholder="Add comment here" v-model="commentText">
+                    <input @focus="showButtons" class="form" placeholder="Add comment here" type="text"
+                           v-model="commentText">
                 </div>
                 <div class="comment-control-buttons hide">
-                    <button @click="addComment" class="btn btn-save" :class="{ disable: !enableSaveButton }" >Save</button>
+                    <button :class="{ disable: !enableSaveButton }" @click="addComment" class="btn btn-save">Save
+                    </button>
                     <button @click="cancelComment" class="btn btn-cancel">Cancel</button>
                 </div>
             </div>
             <div class="line"></div>
             <div class="comments-view">
-                <ul >
-                    <li v-for="comment in reversedCommentsArr" :key="comment.id">
+                <ul>
+                    <li :key="comment.id" v-for="comment in reversedCommentsArr">
                         <Comment :comment="comment" @modal-trigger="triggersHandler"/>
+
                     </li>
                 </ul>
             </div>
@@ -36,12 +40,12 @@
 
 <script>
     import Comment from "@/components/Comment"
-    
+
     export default {
         name: "Comments.vue",
 
-        data(){
-            return{
+        data() {
+            return {
                 commentText: "",
                 commentsArr: [],
                 currentUser: "Illia Piliugin",
@@ -50,70 +54,81 @@
         components: {
             Comment,
         },
-        computed:{
-          enableSaveButton(){
-              if(this.commentText.length){
-                  return true
-              }
-              else {
-                  return false
-              }
-          },
-          reversedCommentsArr(){
-              const arr = this.commentsArr
-              return arr.reverse()
-          }
+        computed: {
+            enableSaveButton() {
+                if (this.commentText.length) {
+                    return true
+                } else {
+                    return false
+                }
+            },
+            reversedCommentsArr() {
+                const arr = this.commentsArr
+                return arr.reverse()
+            }
         },
 
         methods: {
 
+//Close modal window when user press outside modal
+            resetHandler() {
+                const blocker = document.querySelector(".blocker")
+                blocker.style.display = "none"
+                document.querySelectorAll(".modal").forEach(modal => modal.style.display = "none")
 
+            },
             //Handler for modal delete/edit buttons
-            triggersHandler(task, id){
-               // Delete task from array
-               if(task === "delete"){
-                   this.commentsArr.forEach((el,idx) => {
-                       if (el.id === id){
-                           this.commentsArr.splice(idx,1)
-                       }
-                   })
-               }
-               //Edit existing task
-               if (task === "edit"){
-                   const buttons = document.querySelector(".comment-control-buttons")
-                   let commentToEdit = {}
-                   this.commentsArr.forEach((comment,idx) => {
-                       if(parseInt(comment.id) === id){
-                           commentToEdit = comment
-                           this.commentsArr.splice(idx,1)
-                           return 1
-                       }
-                   })
-                   this.commentText = commentToEdit.comment
-                   buttons.classList.remove("hide")
-               }
+            triggersHandler(task, id) {
+                const blocker = document.querySelector(".blocker")
+                blocker.style.display = "none"
+                // Delete task from array
+                if (task === "delete") {
+                    this.commentsArr.forEach((el, idx) => {
+                        if (el.id === id) {
+                            this.commentsArr.splice(idx, 1)
+                        }
+                    })
+                }
+                //Edit existing task
+                if (task === "edit") {
+                    const buttons = document.querySelector(".comment-control-buttons")
+                    let commentToEdit = {}
+                    this.commentsArr.forEach((comment, idx) => {
+                        if (parseInt(comment.id) === id) {
+                            commentToEdit = comment
+                            this.commentsArr.splice(idx, 1)
+                            return 1
+                        }
+                    })
+                    this.commentText = commentToEdit.comment
+                    buttons.classList.remove("hide")
+                }
             },
             //Show save/cancel buttons when focus on input
-            showButtons(){
+            showButtons() {
                 const buttons = document.querySelector(".comment-control-buttons")
                 buttons.classList.remove("hide")
             },
             //Press cancel button
-            cancelComment(){
+            cancelComment() {
                 const buttons = document.querySelector(".comment-control-buttons")
                 this.commentText = ""
                 buttons.classList.add("hide")
             },
             //Add new comment to array
-            addComment(){
+            addComment() {
                 const buttons = document.querySelector(".comment-control-buttons")
-                if (this.commentText !== ""){
-                    const comment = {id:Date.now(), comment:this.commentText, date: Date().substr(0, 15), user: this.currentUser}
+                if (this.commentText !== "") {
+                    const comment = {
+                        id: Date.now(),
+                        comment: this.commentText,
+                        date: Date().substr(0, 15),
+                        user: this.currentUser
+                    }
                     this.commentsArr.push(comment)
                     this.commentText = ""
                     buttons.classList.add("hide")
-                }
-                else alert("Please add comment text")
+                } else alert("Please add comment text")
             },
 
         }
@@ -122,7 +137,7 @@
 
 <style scoped>
 
-    .comments-body{
+    .comments-body {
         background-color: #edfcd2;
         width: 500px;
         min-height: auto;
@@ -132,25 +147,37 @@
         /*border-left-width: 3px;*/
     }
 
+    .blocker {
+        display: none;
+        opacity: 0.3;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        z-index: 10;
+        height: 100%;
+    }
 
-
-    .wrapper{
+    .wrapper {
         margin: 20px 30px;
     }
-    .comments-header{
+
+    .comments-header {
         display: flex;
         justify-content: space-between;
         align-items: center;
     }
-    .think{
+
+    .think {
         color: #61b634;
         line-height: 2;
     }
 
-    .comments-add{
+    .comments-add {
 
     }
-    .form{
+
+    .form {
         width: 100%;
         height: 35px;
         background: none;
@@ -158,63 +185,68 @@
         font-size: 18px;
         transition: 0.3s;
     }
-    .form:focus{
+
+    .form:focus {
         outline: none;
-        margin:10px;
+        margin: 10px;
         color: #000;
     }
-    .comments-view{
+
+    .comments-view {
 
     }
+
     ul {
         list-style: none;
     }
-    .line{
+
+    .line {
         height: 1px;
         background-color: #dcdcdc;
-        width:100%;
+        width: 100%;
         margin-top: 20px;
         margin-bottom: 5px;
 
     }
-/*    Buttons   */
-    .btn{
-        border:none;
+
+    /*    Buttons   */
+    .btn {
+        border: none;
         border-radius: 3px;
         padding: 10px 15px 10px 15px;
         transition: 0.2s;
         text-decoration: none;
         font-weight: bold;
     }
-    .btn-change{
+
+    .btn-change {
         background-color: #61b634;;
         color: #fff;
     }
 
-    .btn-save{
+    .btn-save {
         background-color: #48cccc;
         color: #ffffff;
         margin-right: 15px;
         border: 1px solid #48cccc;
 
     }
-    .btn-cancel{
+
+    .btn-cancel {
         background-color: #ffffff;;
         color: #000;
         border: 1px solid #000;
     }
-    .disable{
+
+    .disable {
         background-color: #b7b5b5;
         color: #fff;
         border: 1px solid #b7b5b5;
     }
 
-    .btn:hover{
-        cursor:pointer;
-        border:1px solid orange;
+    .btn:hover {
+        cursor: pointer;
     }
-
-
 
 
 </style>
